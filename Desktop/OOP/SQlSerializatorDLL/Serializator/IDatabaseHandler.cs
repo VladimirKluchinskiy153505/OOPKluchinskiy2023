00bf8c949ea8d337993abdadb7b4d3c4affd7220
VladimirKluchinskiy153505 <vova.klyuch@gmail.com>
2023-05-16 18:58:12 +0300

    dll serializator via database

---
+++ Desktop/OOP/SQlSerializatorDLL/Serializator/IDatabaseHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Serializator
+{
+    public interface IDatabaseHandler
+    {
+        Task<List<User>> GetUsersAsync();
+        Task<SaveObject> GetSaveByIdAsync(int id);
+        Task<User> GetUserByNameAsync(string name);
+        Task<List<SaveObject>> GetAllSaveDataFromUser(int id);
+        Task Init();
+        Task<int> AddUserAsync(User user);
+        Task<int> AddSaveDataAsync(SaveObject saveData);
+        Task<int> EditSaveDataAsync(SaveObject saveData);
+        Task<bool> UserExistsAsync(string userName);
+        Task<Dictionary<int, GameData>> LoadAllProfiles(int currentUserId);
+        Task Save(GameData gameData, int userId, int saveId);
+        Task<int> CreateNewGame(GameData gameData, int userId);//return new Save Id
+        Task<GameData> Load(int saveId);
+    }
+
+}
---
+++ Desktop/OOP/SQlSerializatorDLL/Serializator/UserData.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+
+namespace Serializator
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement, Indexed]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+    [Table("Saves")]
+    public class SaveObject
+    {
+        [PrimaryKey, AutoIncrement, Indexed]
+        public int Id { get; set; }
+        public int LevelNum { get; set; }
+        public int CharacterLives { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public string HeartsCollected { get; set; }
+        public int UserId { get; set; }
+    }
+
+}
